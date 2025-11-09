16311265b5444de096f5c14a47ee01e734bc0a4b
Sathvik <sathvikkulal111@gmail.com>
2025-11-09 19:03:46 +0530

    mark-progress, mark-done, list functions implemented

--- task-tracker-cli.py
+++ task-tracker-cli.py
@@ -18,9 +18,9 @@ def main():
         case "add": add_task()
         case "delete": delete_task()
         case "update": update_task()
-        # case "mark-in-progress": mark_in_progress()
-        # case "mark-done": mark_done()
-        # case "list": show_tasks()
+        case "mark-in-progress": mark_in_progress()
+        case "mark-done": mark_done()
+        case "list": list_tasks()
         case _ : sys.exit("Wrong command: Please refer correct command")
     
     with open("count.txt", "w") as file:
@@ -37,7 +37,7 @@ def add_task():
         task_prop = {
                         "id": id,
                         "description": task_name,
-                        "status": "to_do",
+                        "status": "to-do",
                         "createdAt": date.today().isoformat(),
                         "updatedAt": None
                     }
@@ -101,8 +101,82 @@ def update_task():
         for line in lines:
             if line["id"] == task_id:
                 line["description"]= new_task_name
-                data.append(line)
+                line["updatedAt"] = date.today().isoformat()
+            data.append(line)
         with open("tasks.json", "w") as file:
             json.dump(data, file)
+
+def mark_in_progress():
+    try:
+        task_id = int(sys.argv[2])
+        with open("tasks.json", "r") as file:
+            lines = json.load(file)
+            print(lines)
+    except IndexError:
+        sys.exit("Missing task id")
+    except json.decoder.JSONDecodeError:
+        print("Tasks list is empty.")
+        pass
+    except ValueError:
+        sys.exit("Enter a valid id")
+
+    else:
+        data = []
+        for line in lines:
+            if line["id"] == task_id:
+                line["status"]= "In-progress"
+                line["updatedAt"] = date.today().isoformat()
+            data.append(line)
+        with open("tasks.json", "w") as file:
+            json.dump(data, file)
+
+def mark_done():
+    try:
+        task_id = int(sys.argv[2])
+        with open("tasks.json", "r") as file:
+            lines = json.load(file)
+            print(lines)
+    except IndexError:
+        sys.exit("Missing task id")
+    except json.decoder.JSONDecodeError:
+        print("Tasks list is empty.")
+        pass
+    except ValueError:
+        sys.exit("Enter a valid id")
+
+    else:
+        data = []
+        for line in lines:
+            if line["id"] == task_id:
+                line["status"]= "done"
+                line["updatedAt"] = date.today().isoformat()
+            data.append(line)
+        with open("tasks.json", "w") as file:
+            json.dump(data, file)
+
+def list_tasks():
+    try:
+        with open("tasks.json", "r") as file:
+            lines = json.load(file)
+            print(lines)
+    except IndexError:
+        pass
+    except json.decoder.JSONDecodeError:
+        print("Tasks list is empty.")
+        pass
+    except ValueError:
+        pass
+    else:
+        if len(sys.argv) == 2:
+            status = None
+        else: 
+            status = sys.argv[2].lower()
+        if status is None:
+            for line in lines:
+                print(f" Task id: {line["id"]} | Description: {line["description"]} | Status: {line["status"]} | Creation Date: {line["createdAt"]} | Updated Date: {line["updatedAt"]}")
+        else:
+            for line in lines:
+                if line["status"].lower() == status:
+                    print(f" Task id: {line["id"]} | Description: {line["description"]} | Creation Date: {line["createdAt"]} | Updated Date: {line["updatedAt"]}")
 
 main()
